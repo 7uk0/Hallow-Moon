41b12b72466db46c6e101bca9871bc1aea4b0554
Quasar <dillonj4682@gmail.com>
2025-08-01 23:24:15 -0500

    Rotation and Local Position

--- scripts/Player/o_bebop.gd
+++ scripts/Player/o_bebop.gd
@@ -1,35 +1,46 @@
 extends Node2D
-
 var fuel
-var fuel_bar
+var fuel_perc
 var speed = 30
 const MAX_FUEL = 100
-const FUEL_CONSUMPTION_RATE = 10 
-@export var fuel_bar_path: NodePath
+var rotationSpeed = 1
+var input_vector = Vector2.ZERO
+const FUEL_CONSUMPTION_RATE = 2 
+@export var fuel_percentage_path: NodePath 
 
 func _ready():
 	fuel = MAX_FUEL
-	fuel_bar = get_node(fuel_bar_path)
-	
+	fuel_perc = get_node(fuel_percentage_path)
+
 func _process(delta): 
-	var moved = false
-
+	
+	#Movement Mechanics
+	var moving = false
 	if Input.is_key_pressed(KEY_W):
-		position.y -= speed * delta
-		moved = true
+		input_vector.y -= 1
+		moving = true
 	if Input.is_key_pressed(KEY_S):
-		position.y += speed * delta
-		moved = true
+		input_vector.y += 1
+		moving = true
 	if Input.is_key_pressed(KEY_D):
-		position.x += speed * delta
-		moved = true
+		input_vector.x -= 1
+		moving = true
 	if Input.is_key_pressed(KEY_A):
-		position.x -= speed * delta
-		moved = true
-
-	if moved and fuel > 0:
+		input_vector.x += 1
+		moving = true
+	if input_vector.x != 0 or input_vector.y != 0:
+		input_vector = input_vector.normalized()
+		
+	if Input.is_key_pressed(KEY_E):
+		rotation += rotationSpeed * delta
+	if Input.is_key_pressed(KEY_Q):
+		rotation -= rotationSpeed * delta
+		
+	var local_direction = input_vector.rotated(rotation)
+	if moving : position += local_direction * speed * delta
+	if fuel <= 0: speed = 0
+	if moving and fuel > 0:
 		fuel -= FUEL_CONSUMPTION_RATE * delta
 		fuel = max(fuel, 0)
-
-	if fuel <= 0: speed = 0  
-	fuel_bar.value = fuel
+		fuel_perc.text = str(int((fuel / MAX_FUEL) * 100)) + "%"
+	
